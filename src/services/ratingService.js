89ecbe42353b5c13b61387b78f5fecf4628d9bf2
Zhi-Yan,Haung <w0975582420@gmail.com>
2024-12-05 22:30:43 +0800

    feat: add get ratings with user info by host_id API

--- src/services/ratingService.js
+++ src/services/ratingService.js
@@ -1,11 +1,35 @@
-import { GetRatingSchema } from "../validations/ratingSchema.js"
-import { prisma } from "../config/db.js"
+import { GetRatingSchema } from "../validations/ratingSchema.js";
+import { prisma } from "../config/db.js";
 
 export const ratingService = {
   async getRatingByHostId(host_id) {
-    GetRatingSchema.parse({host_id})
+    GetRatingSchema.parse({ host_id });
     return await prisma.ratings.findMany({
       where: { host_id },
-    })
+    });
   },
-}
+
+  async getRatingWithUserInfo(host_id) {
+    GetRatingSchema.parse({ host_id });
+    return await prisma.ratings.findMany({
+      where: { host_id },
+      select: {
+        rating_id: true,
+        user_id: true,
+        rating_heart: true,
+        user_comment: true,
+        rating_kindness: true,
+        rating_ability: true,
+        rating_credit: true,
+        created_at: true,
+        users_ratings_host_idTousers: {
+          select: {
+            display_name: true,
+            city: true,
+            photo_url: true,
+          },
+        },
+      },
+    });
+  },
+};
