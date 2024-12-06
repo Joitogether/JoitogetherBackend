f32a4cebad1de69b177f830626cf6b3a20a14f45
Zhi-Yan,Haung <w0975582420@gmail.com>
2024-12-06 19:35:15 +0800

    feat: 📰 add API to fetch all posts

--- src/routes/followerRoutes.js
+++ src/routes/followerRoutes.js
@@ -1,3 +1,25 @@
 import express from "express";
 import { followerService } from "../services/followerService.js";
 const router = express.Router();
+
+// 查詢 user_id 的追隨者
+router.get("/:user_id", async (req, res, next) => {
+  try {
+    const { user_id } = req.params;
+    const response = await followerService.getFollowersByUserId(user_id);
+    if (!response || response.length === 0) {
+      return res.status(404).json({
+        message: "查無此資料",
+        status: 404,
+      });
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: "資料獲取成功",
+      data: response,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,42 +1,49 @@
-import { prisma } from '../config/db.js'
-import { CreatePostCommentSchema, CreatePostSchema, GetCategoryPostSchema, GetPostSchema } from '../validations/postSchema.js'
+import { prisma } from "../config/db.js";
+import {
+  CreatePostCommentSchema,
+  CreatePostSchema,
+  GetCategoryPostSchema,
+  GetPostSchema,
+} from "../validations/postSchema.js";
 
-
-export  const postService = {
-  async createPost(data){
-    CreatePostSchema.parse(data)
+export const postService = {
+  async createPost(data) {
+    CreatePostSchema.parse(data);
     return await prisma.posts.create({
-      data
-    })
+      data,
+    });
   },
-  async createPostComment(data){
-    CreatePostCommentSchema.parse(data)
+  async createPostComment(data) {
+    CreatePostCommentSchema.parse(data);
     return await prisma.post_comments.create({
-      data
-    })
+      data,
+    });
+  },
+  async getAllPosts() {
+    return await prisma.posts.findMany();
   },
 
-  async getPost(post_id){
-    GetPostSchema.parse({post_id})
+  async getPost(post_id) {
+    GetPostSchema.parse({ post_id });
     return await prisma.posts.findUnique({
-      where: { post_id }
-    })
+      where: { post_id },
+    });
   },
 
-  async getPostByCategory(post_category){
-    GetCategoryPostSchema.parse({post_category})
+  async getPostByCategory(post_category) {
+    GetCategoryPostSchema.parse({ post_category });
     const response = await prisma.posts.findMany({
-      where: { 
+      where: {
         post_category,
-        post_status: 'posted'
+        post_status: "posted",
       },
       orderBy: {
-        created_at: 'desc'
-      }
-    })
-    if(response.length === 0){
-      return null
+        created_at: "desc",
+      },
+    });
+    if (response.length === 0) {
+      return null;
     }
-    return response
-  }
-}
+    return response;
+  },
+};
