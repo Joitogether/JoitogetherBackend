39ba28ba8dce8d8fa7f212d1fed967d7c3fcbaff
Zhi-Yan,Haung <w0975582420@gmail.com>
2024-12-06 14:32:18 +0800

    feat: add get ratings with user info by user_id API

--- src/routes/ratingRoutes.js
+++ src/routes/ratingRoutes.js
@@ -23,11 +23,32 @@ router.get("/:host_id", async (req, res, next) => {
 });
 
 // 獲得評論者詳細資料（含 city display_name photo_url）
-router.get("/details/:host_id", async (req, res, next) => {
+router.get("/hostDetails/:host_id", async (req, res, next) => {
   try {
     const { host_id } = req.params;
-    const response = await ratingService.getRatingWithUserInfo(host_id);
+    const response = await ratingService.getRatingWithUserInfoByHostId(host_id);
 
+    if (!response || response.length === 0) {
+      return res.status(404).json({
+        status: 404,
+        message: "查無此資料",
+      });
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: "資料獲取成功",
+      data: response,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/userDetails/:user_id", async (req, res, next) => {
+  try {
+    const { user_id } = req.params;
+    const response = await ratingService.getRatingWithUserInfoByUserId(user_id);
     if (!response || response.length === 0) {
       return res.status(404).json({
         status: 404,
