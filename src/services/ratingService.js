5edd32f6b37b5ed5b8f96436d163d2a030057ff0
Zhi-Yan,Haung <w0975582420@gmail.com>
2024-12-06 14:34:57 +0800

    feat: add get ratings with user info by user_id API

--- src/services/ratingService.js
+++ src/services/ratingService.js
@@ -9,7 +9,8 @@ export const ratingService = {
     });
   },
 
-  async getRatingWithUserInfo(host_id) {
+  // 查詢 host_id（被評價者） 的評論
+  async getRatingWithUserInfoByHostId(host_id) {
     GetRatingSchema.parse({ host_id });
     return await prisma.ratings.findMany({
       where: { host_id },
@@ -25,8 +26,33 @@ export const ratingService = {
         users_ratings_host_idTousers: {
           select: {
             display_name: true,
-            city: true,
             photo_url: true,
+            city: true,
+          },
+        },
+      },
+    });
+  },
+
+  // 查詢 user_id（評價者） 的評論
+  async getRatingWithUserInfoByUserId(user_id) {
+    GetRatingSchema.parse({ user_id });
+    return await prisma.ratings.findMany({
+      where: { user_id },
+      select: {
+        rating_id: true,
+        host_id: true,
+        rating_heart: true,
+        user_comment: true,
+        rating_kindness: true,
+        rating_ability: true,
+        rating_credit: true,
+        created_at: true,
+        users_ratings_host_idTousers: {
+          select: {
+            display_name: true,
+            photo_url: true,
+            city: true,
           },
         },
       },
