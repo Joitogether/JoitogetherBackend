93b564ebf9cf83a5be08345a3e2bd3a8e393e748
Zhi-Yan,Haung <w0975582420@gmail.com>
2024-12-06 19:36:41 +0800

    feat: 🧑‍🤝‍🧑 add API to fetch user's followers

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,9 @@
-import { prisma } from '../config/db.js';
-import {  UserUidSchema } from '../validations/userSchema.js';
+import { prisma } from "../config/db.js";
+import { UserUidSchema } from "../validations/userSchema.js";
+import {
+  GetFollowersSchema,
+  GetFollowingSchema,
+} from "../validations/userSchema.js";
 
 export const userService = {
   async getAllUsers() {
@@ -12,9 +16,9 @@ export const userService = {
     });
   },
 
-  async getUserByEmail( email) {
+  async getUserByEmail(email) {
     return await prisma.users.findUnique({
-      where: {  email },
+      where: { email },
       select: {
         display_name: true,
         email: true,
@@ -22,33 +26,31 @@ export const userService = {
         full_name: true,
         phone_number: true,
         photo_url: true,
-        uid: true
-      }
+        uid: true,
+      },
     });
   },
 
   async userRegister(userData) {
     return await prisma.users.create({
-      data: userData
+      data: userData,
     });
   },
-  
+
   async userUpdateInfo(userData, uid) {
     return await prisma.users.update({
       where: { uid },
-      data: userData
+      data: userData,
     });
   },
-  
+
   async getApplicationsByUserId(uid) {
-    UserUidSchema.parse({ uid })
-    const response =  await prisma.applications.findMany({
-      where: { participant_id: uid,
-        OR: [
-          { status: 'registered' },
-          { status: 'approved' }
-        ]
-       },
+    UserUidSchema.parse({ uid });
+    const response = await prisma.applications.findMany({
+      where: {
+        participant_id: uid,
+        OR: [{ status: "registered" }, { status: "approved" }],
+      },
       include: {
         activities: {
           select: {
@@ -56,26 +58,61 @@ export const userService = {
             location: true,
             event_time: true,
             img_url: true,
-          }
-        }
-      }
+          },
+        },
+      },
     });
-    if(response.length === 0){
-      return null
+    if (response.length === 0) {
+      return null;
     }
-    return response
+    return response;
   },
+
   async getUserPosts(uid) {
-    UserUidSchema.parse({ uid })
+    UserUidSchema.parse({ uid });
     const response = await prisma.posts.findMany({
-      where: { uid, post_status: 'posted' },
+      where: { uid, post_status: "posted" },
       orderBy: {
-        created_at: 'desc'
-      }
-    })
-    if(!response || response.length === 0){
-      return null
+        created_at: "desc",
+      },
+    });
+    if (!response || response.length === 0) {
+      return null;
     }
-    return response
-  }
-}
+    return response;
+  },
+
+  async getFollowersByUserId(user_id) {
+    GetFollowersSchema.parse({ user_id });
+    return await prisma.followers.findMany({
+      where: { user_id },
+      select: {
+        follower_id: true,
+        users_followers_follower_idTousers: {
+          select: {
+            display_name: true,
+            photo_url: true,
+            favorite_sentence: true,
+          },
+        },
+      },
+    });
+  },
+
+  async getFollowingByFollowerId(follower_id) {
+    GetFollowingSchema.parse({ follower_id });
+    return await prisma.followers.findMany({
+      where: { follower_id },
+      select: {
+        user_id: true,
+        users_followers_user_idTousers: {
+          select: {
+            display_name: true,
+            photo_url: true,
+            favorite_sentence: true,
+          },
+        },
+      },
+    });
+  },
+};
--- src/validations/userSchema.js
+++ src/validations/userSchema.js
@@ -1,4 +1,4 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 export const UserCreateSchema = z.object({
   uid: z.string().max(255),
@@ -7,15 +7,21 @@ export const UserCreateSchema = z.object({
   full_name: z.string().max(255),
   display_name: z.string().max(255),
   phone_number: z.string().regex(/^09\d{8}$/),
-  photo_url: z.string().optional()
-})
+  photo_url: z.string().optional(),
+});
 
 export const UserUpdateSchema = z.object({
-  email: z.string().regex(/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/).optional(),
+  email: z
+    .string()
+    .regex(/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/)
+    .optional(),
   email_verified: z.boolean().optional(),
   full_name: z.string().max(255).optional(),
   display_name: z.string().max(255).optional(),
-  phone_number: z.string().regex(/^09\d{8}$/).optional(),
+  phone_number: z
+    .string()
+    .regex(/^09\d{8}$/)
+    .optional(),
   photo_url: z.string().optional(),
   city: z.string().max(5).optional(),
   age: z.number().int().optional(),
@@ -29,8 +35,17 @@ export const UserUpdateSchema = z.object({
   interested_in: z.string().max(255).optional(),
   life_photo_1: z.string().optional(),
   life_photo_2: z.string().optional(),
-})
+});
 
 export const UserUidSchema = z.object({
-  uid: z.string().max(255).min(5)
-})
+  uid: z.string().max(255).min(5),
+});
+
+// followers table
+export const GetFollowersSchema = z.object({
+  user_id: z.string().max(255),
+});
+
+export const GetFollowingSchema = z.object({
+  follower_id: z.string().max(255),
+});
