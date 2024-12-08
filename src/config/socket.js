8736b72bac104b9bc8c8fb0f711ea6ee3d1f9d99
Junwanghere <change60201@gmail.com>
2024-12-08 14:07:01 +0800

    feat: add link column and get target detail when getting notification

--- src/config/socket.js
+++ src/config/socket.js
@@ -19,13 +19,15 @@ export const initSocket = (server) => {
     })
     // 聽到sendNotification時就根據挾帶的data裡面是否有帶要給這個資訊人的id，把data打到那個通道
     socket.on('sendNotification', async (data) => {
+      //拿到提醒先新增在資料庫
       const response = await userService.addNotification(data)
+      // 成功新增資料庫之後將提醒直接發給該使用者
       console.log(response)
       // io.to(data.receiverId).emit('newNotification', data);
       if(!response){
         return
       }
-      io.to(data.user_id).emit('newNotification', data)
+      io.to(data.user_id).emit('newNotification', response)
 
     });
   })
