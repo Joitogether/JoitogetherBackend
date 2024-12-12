d9fd0cc8868046310f72a0d5fd583ef0bd3ba4f2
Zhi-Yan,Haung <w0975582420@gmail.com>
2024-12-12 12:12:00 +0800

    refactor: 🔧📂 split index.js into app.js and service.js

---
+++ app.js
@@ -0,0 +1,28 @@
+// import express from "express";
+// import activityRouter from "./src/routes/activityRoutes.js";
+// import userRouter from "./src/routes/userRoutes.js";
+// import applicationRouter from "./src/routes/applicationRoutes.js";
+// import postRouter from "./src/routes/postRoutes.js";
+// import ratingRouter from "./src/routes/ratingRoutes.js";
+// import cors from "cors";
+// import { errorMiddleware } from "./src/middlewares/errorMiddleware.js";
+// import { initSocket } from "./src/config/socket.js";
+
+// const app = express();
+
+// // middlewares
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+
+// // routes
+// app.use("/activities", activityRouter);
+// app.use("/users", userRouter);
+// app.use("/applications", applicationRouter);
+// app.use("/posts", postRouter);
+// app.use("/ratings", ratingRouter);
+
+// // error
+// app.use(errorMiddleware);
+
+// export { app };
---
+++ service.js
@@ -0,0 +1,17 @@
+// import { app } from "./app.js";
+// import http from "http";
+// import { Server } from "socket.io";
+
+// const port = 3030;
+
+// const server = http.createServer(app);
+// initSocket(server);
+
+// server.listen(port, () => {
+//   console.log(`server running on port ${port}`);
+// });
+
+// // server start
+// // app.listen(port,'0.0.0.0', () => {
+// //   console.log(`server running on port ${port}`)
+// // })
