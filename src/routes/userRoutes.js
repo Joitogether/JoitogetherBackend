de1fc2d0d96b7948516a0eed0d5e67c05c81f56a
Zhi-Yan,Haung <w0975582420@gmail.com>
2024-12-25 16:28:23 +0800

    fix: resolve 404 issue on URL reload for deployment paths

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,25 +4,64 @@ import * as UserController from "../controllers/userController.js";
 const router = express.Router();
 
 // User List
-router.get("/", UserController.fetchAllUsers);
-router.get("/:uid", UserController.fetchUserById);
+router.get(
+  "/",
+  /* #swagger.tags = ['User'] */
+  UserController.fetchAllUsers
+);
+router.get(
+  "/:uid",
+  /* #swagger.tags = ['User'] */ UserController.fetchUserById
+);
 
 // User Create / Update
-router.post("/register", UserController.registerUser);
-router.put("/update/:uid", UserController.updateUserInfo);
+router.post(
+  "/register",
+  /* #swagger.tags = ['User'] */
+  UserController.registerUser
+);
+router.put(
+  "/update/:uid",
+  /* #swagger.tags = ['User'] */
+  UserController.updateUserInfo
+);
 
 // User Application
-router.get("/applications/:uid", UserController.fetchUserApplications);
+router.get(
+  "/applications/:uid",
+  /* #swagger.tags = ['User'] */
+  UserController.fetchUserApplications
+);
 
 // User Post
-router.get("/posts/:uid", UserController.fetchUserPosts);
+router.get(
+  "/posts/:uid",
+  /* #swagger.tags = ['User'] */
+  UserController.fetchUserPosts
+);
 
 // User Notification
-router.get("/notifications/:uid", UserController.fetchUserNotifications);
-router.put("/notifications/:uid", UserController.markUserNotifications);
+router.get(
+  "/notifications/:uid",
+  /* #swagger.tags = ['User'] */
+  UserController.fetchUserNotifications
+);
+router.put(
+  "/notifications/:uid",
+  /* #swagger.tags = ['User'] */
+  UserController.markUserNotifications
+);
 
 // User Followers
-router.get("/userFollowers/:user_id", UserController.fetchUserFollowers);
-router.get("/following/:follower_id", UserController.fetchUserFollowing);
+router.get(
+  "/userFollowers/:user_id",
+  /* #swagger.tags = ['User'] */
+  UserController.fetchUserFollowers
+);
+router.get(
+  "/following/:follower_id",
+  /* #swagger.tags = ['User'] */
+  UserController.fetchUserFollowing
+);
 
 export default router;
