0e4707ebfabff5dfe8ebaa8e81065b1adb410aaf
Junwanghere <change60201@gmail.com>
2024-12-26 11:37:30 +0800

    fix: change get category route from get to post

--- src/routes/activityRoutes.js
+++ src/routes/activityRoutes.js
@@ -5,13 +5,13 @@ const router = express.Router();
 // Activity List
 router.get("/", ActivityController.fetchAllActiveActivities);
 router.get("/:id", ActivityController.fetchActivityDetails);
-router.get("/category/:type", ActivityController.fetchActivitiesByCategory);
 
 
 // Activity Create/Delete
 router.post("/", ActivityController.addNewActivity);
 router.put("/cancel/:id", ActivityController.cancelActivityRequest);
 router.post("/search", ActivityController.searchActivities);
+router.post("/category/:type", ActivityController.fetchActivitiesByCategory);
 
 // Activity Comment
 router.post("/comment/:activity_id", ActivityController.fetchActivityComments);
