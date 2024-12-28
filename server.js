959977b52c03264a9c0f08b23e7a2d50d68cc685
Junwanghere <change60201@gmail.com>
2024-12-28 20:10:15 +0800

    feat: cronjob of changing status of registration activities and send notifications to participants

--- server.js
+++ server.js
@@ -2,28 +2,14 @@ import { app } from "./app.js";
 import http from "http";
 import "dotenv/config";
 import { initSocket } from "./src/config/socket.js";
-import cron from 'node-cron'
-import { prisma }  from './src/config/db.js'
-import dayjs from 'dayjs'
-
-
-
-cron.schedule('*/5 * * * * *', () => {
-  //先抓有沒有昨天進行的活動
-  console.log('running a task every minute');
-  // 將活動狀態改為completed
-  console.log(dayjs()) 
-  // 抓這個活動的參加者送提醒給他們
-
-},{
-  timezone: 'Asia/Taipei'
-});
-
+import cronJobs from "./src/utils/cronjob.js";
 
 const port = 3030;
 
 const server = http.createServer(app);
 initSocket(server);
+
+cronJobs.dailyUpdates();
 
 server.listen(port, () => {
   console.log(`server running on port ${port}`);
