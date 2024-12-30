be77423059f7204c9595df091516e997fb3bda13
Zhi-Yan,Haung <w0975582420@gmail.com>
2024-12-30 15:53:28 +0800

    fix: resolve merge conflicts

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,3 +1,4 @@
+import { createSesEncrypt, createShaEncrypt } from "../utils/payment.js";
 import paymentService from "../services/paymentService.js";
 import { orderService } from "../services/orderService.js";
 import { cartService } from "../services/cartService.js";
@@ -5,6 +6,32 @@ import { activityService } from "../services/activityService.js";
 import { PaymentSchema } from "../validations/paymentSchema.js";
 import "dotenv/config";
 
+const paymentEncrytOrder = async (req, res, next) => {
+  try {
+    const data = req.body;
+    const TimeStamp = Math.round(new Date().getTime() / 1000);
+    const order = {
+      ...data,
+      TimeStamp,
+      Amt: parseInt(data.Amount),
+      MerchantOrderNo: TimeStamp,
+    };
+
+    const aesEncrypt = createSesEncrypt(order);
+    const shaEncrypt = createShaEncrypt(aesEncrypt);
+    order.aesEncrypt = aesEncrypt;
+    order.shaEncrypt = shaEncrypt;
+
+    res.status(200).json({
+      status: 200,
+      message: "加密成功",
+      data: order,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
 
 // 儲值
 const paymentDeposit = async (req, res, next) => {
@@ -182,4 +209,5 @@ export {
   fetchTransactionHistory,
   decreaseBalance,
   handleCheckoutProcess,
+  paymentEncrytOrder,
 };
